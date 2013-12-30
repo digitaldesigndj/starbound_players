65a1815db3fe70ee4301330e824bd42161aa9fe9
root <root@boundstar.com>
2013-12-30 04:19:09 -0500

    starbound log reader

---
+++ app.js
@@ -0,0 +1,74 @@
+// define your terminator for easy reference, changes
+var msgTerminator = '\n';
+
+
+    // see if there is one or more complete messages
+    if (buf.indexOf(msgTerminator) >= 0) {
+        // slice up the buffer into messages
+        var msgs = data.split(msgTerminator);
+
+        for (var i = 0; i < msgs.length - 2; ++i) {
+            // walk through each message in order
+            var msg = msgs[i];
+
+            // pick off the current message
+            console.log('Data in server, sending to handle()');
+            // send only the current message to your handler
+            worker.handle(msg, socket);
+        }
+
+        buf = msgs[msgs.length - 1];  // put back any partial message into your buffer
+    }
+});
+
+var fs = require('fs'),
+    bite_size = 256,
+    readbytes = 0,
+    file;
+
+fs.open('/root/Starbound/linux64/starbound_server.log', 'r', function(err, fd) { file = fd; readsome(); });
+
+function readsome() {
+    var stats = fs.fstatSync(file); // yes sometimes async does not make sense!
+    if(stats.size<readbytes+1) {
+    	console.log('---------------- TICK ------------' );
+        setTimeout(readsome, 3000);
+    }
+    else {
+        fs.read(file, new Buffer(bite_size), 0, bite_size, readbytes, processsome);
+    }
+}
+
+function processsome(err, bytecount, buff) {
+	var regex = \Info:\sClient\s'(.*?)\sdisconnected\
+
+	console.log( buff.toString() 
+
+
+
+/*
+	if ( buff.toString().indexOf(msgTerminator) >= 0 ) {
+        // slice up the buffer into messages
+        var msgs = data.split(msgTerminator);
+        console.log( msgs );
+    }
+*/
+    
+    // console.log( buff.toString().split('Info: Client ') );
+    
+    
+    //console.log('Read', bytecount, 'and will process it now.');
+
+    // Here we will process our incoming data:
+        // Do whatever you need. Just be careful about not using beyond the bytecount in buff.
+//         console.log( buff.toString( 'utf-8', 0, bytecount ).split("\n") );
+/* 		console.log( buff.toString( 'ascii' ) ); */
+		
+		
+		// .split(/\r\n|[\n\r\u0085\u2028\u2029]/g) );
+        // console.log( buff.toString( 'utf-8', 0, bytecount ).split(/\r\n|[\n\r\u0085\u2028\u2029]/g) );
+
+    // So we continue reading from where we left:
+    readbytes+=bytecount;
+    process.nextTick(readsome);
+}
---
+++ player.js
@@ -0,0 +1,63 @@
+fs = require('fs');
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+update_players_list();
+setInterval( update_players_list, 30000 );
+
+function update_players_list() {
+	data = fs.readFileSync('/root/Starbound/linux64/starbound_server.log')
+	
+	lines = data.toString().split("\n");
+	
+	players_on = [];
+	
+	for ( i = 0; i < lines.length - 1; ++i ) {
+		// console.log( i );
+		// console.log( lines[i] );
+		// console.log( lines[i].indexOf("Info: Client ") );
+		if( lines[i].indexOf("Info: Client ") === 0 ) {
+	/*
+			console.log( i );
+			console.log( lines[i] );
+			console.log( lines[i].split(' ').length );
+			console.log( msg[msg.length-1] );
+	*/
+			msg = lines[i].split(' ');
+			if( msg[msg.length-1] === 'connected' ) {
+				// console.log( lines[i] );
+				// console.log( get_user( lines[i] ) );
+				players_on.push( get_user( lines[i] ) );
+			}
+			if( msg[msg.length-1] === 'disconnected' ) {
+				// console.log( lines[i] );
+				players_on.remove( get_user( lines[i] ) );
+			}
+			
+		}
+	}
+	
+	fs.writeFile( '/var/www/boundstar.com/public_html/players.json', JSON.stringify( players_on , null, 4 ), function( err ) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("JSON players file saved");
+		}
+	}); 
+}
+
+function get_user( string ) {
+	var start_pos = string.indexOf('\'') + 1;
+	var end_pos = string.indexOf('\'',start_pos);
+	return string.substring(start_pos,end_pos);
+}
+
